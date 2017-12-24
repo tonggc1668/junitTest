a6a3de302e0d339a946400437faa2a398e1947b2
tonggc1668 <tonggc1668@163.com>
2017-12-24 18:03:55 +0800

    第一次提交

---
+++ junitTest/src/main/java/com/test/junitTest/ICalcMethod.java
@@ -0,0 +1,4 @@
+package com.test.junitTest;
+public interface ICalcMethod {
+	  double calc(Position position);
+	} 
---
+++ junitTest/src/main/java/com/test/junitTest/Position.java
@@ -0,0 +1,4 @@
+package com.test.junitTest;
+public enum Position {
+  BOSS, PROGRAMMER, SURFER
+}
